4e88c7f2c46b7db192d1c0ee334ef3d37441fa57
Antoni <antonig@sas.upenn.edu>
2018-03-27 04:03:10 -0400

    Basic calculator done without sales tax

---
+++ calc.js
@@ -0,0 +1,30 @@
+$('#price').val('0.00');
+$('#shipping').val('0.00');
+$( "#price" ).change(function() {
+  	adjustPrice();
+});
+$( "#shipping" ).change(function() {
+  	adjustPrice();
+});
+
+function adjustPrice() {
+	var raw = parseFloat($('#price').val()) + parseFloat($('#shipping').val());
+	var regex=/^[0-9]+$/;
+	if (!isNaN(raw)) {
+		var tcgFee =  raw*.1025;
+		var paypalFee = raw*.025;
+		var totalFees = tcgFee + paypalFee + .30;
+		$('#finalFee').val(roundTo(totalFees,2));
+		$('#profit').val(roundTo(raw - totalFees, 2));
+	}
+}
+
+function roundTo(n, digits) {
+    if (digits === undefined) {
+      digits = 0;
+    }
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    var test =(Math.round(n) / multiplicator);
+    return +(test.toFixed(digits));
+}
