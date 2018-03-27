8820288135c6d8a390a63e5a896226dc5d384cfc
Antoni <antonig@sas.upenn.edu>
2018-03-27 04:24:43 -0400

    fixed round

--- calc.js
+++ calc.js
@@ -20,13 +20,21 @@ function adjustPrice() {
 }
 
 function roundTo(n, digits) {
+    var negative = false;
     if (digits === undefined) {
-      digits = 0;
+        digits = 0;
+    }
+        if( n < 0) {
+        negative = true;
+      n = n * -1;
     }
     var multiplicator = Math.pow(10, digits);
     n = parseFloat((n * multiplicator).toFixed(11));
-    var test =(Math.round(n) / multiplicator);
-    return +(test.toFixed(digits));
+    n = (Math.round(n) / multiplicator).toFixed(2);
+    if( negative ) {    
+        n = (n * -1).toFixed(2);
+    }
+    return n;
 }
 
 function getTax() {
